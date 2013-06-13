746f36ec057b7e9e737fba857e2ec91180d882f3
Ben Damman <ben.damman@gmail.com>
2013-06-12 21:39:26 -0500

    Manually migrated static css, js, and html into rails assets and view templates.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,150 +1,128 @@
 $(function(){
-	// Expanding options box.
+	// Expanding help box.
 	$('.title-bar').click(function() {
-		$('.options').slideToggle();
+		$('.help').slideToggle();
 	});
 
-
 	// Create and initialize variables.
-	var petitionData = [],
-	petitions = [], // this will be an array containing the title and ID
-	ENDPOINT = 'https://api.whitehouse.gov/v1/petitions.jsonp',
-	timestamps = [],
-	allTrends = [],
-	issues = {'0': 'All',
-			  '1': 'Agriculture',
-			  '2': 'Arts and Humanities',
-			  '3': 'Budget and Taxes',
-			  '4': 'Civil Rights and Liberties',
-			  '8': 'Climate Change',
-			  '9': 'Consumer Protections',
-			  '10': 'Criminal Justice and Law Enforcement',
-			  '12': 'Defense',
-			  '13': 'Disabilities',
-			  '16': 'Economy',
-			  '18': 'Education',
-			  '19': 'Energy',
-			  '20': 'Environment',
-			  '21': 'Family',
-			  '22': 'Firearms',
-			  '24': 'Foreign Policy',
-			  '193': 'Government Reform',
-			  '25': 'Health Care',
-			  '26': 'Homeland Security and Disaster Relief',
-			  '27': 'Housing',
-			  '28': 'Human Rights',
-			  '29': 'Immigration',
-			  '30': 'Innovation',
-			  '97': 'Job Creation',
-			  '103': 'Labor',
-			  '109': 'Natural Resources',
-			  '115': 'Postal Service',
-			  '121': 'Poverty',
-			  '127': 'Regulatory Reform',
-			  '133': 'Rural Policy',
-			  '139': 'Science and Space Policy',
-			  '151': 'Small Business',
-			  '145': 'Social Security',
-			  '157': 'Technology and Telecommunications',
-			  '163': 'Trade',
-			  '169': 'Transportation and Infrastructure',
-			  '175': 'Urban Policy',
-			  '181': 'Veterans and Military Families',
-			  '187': "Woman's Issues"};
+	var timeSeriesData,
+	timeSeriesAPI = 'http://wethedata.herokuapp.com/issues';
 
-	console.log("Issues: " + issues);
+	/*
+	// Issues array... just keeping it here for reference, for now.
+	var issues = {'0': 'All',
+				  '1': 'Agriculture',
+				  '2': 'Arts and Humanities',
+				  '3': 'Budget and Taxes',
+				  '4': 'Civil Rights and Liberties',
+				  '8': 'Climate Change',
+				  '9': 'Consumer Protections',
+				  '10': 'Criminal Justice and Law Enforcement',
+				  '12': 'Defense',
+				  '13': 'Disabilities',
+				  '16': 'Economy',
+				  '18': 'Education',
+				  '19': 'Energy',
+				  '20': 'Environment',
+				  '21': 'Family',
+				  '22': 'Firearms',
+				  '24': 'Foreign Policy',
+				  '193': 'Government Reform',
+				  '25': 'Health Care',
+				  '26': 'Homeland Security and Disaster Relief',
+				  '27': 'Housing',
+				  '28': 'Human Rights',
+				  '29': 'Immigration',
+				  '30': 'Innovation',
+				  '97': 'Job Creation',
+				  '103': 'Labor',
+				  '109': 'Natural Resources',
+				  '115': 'Postal Service',
+				  '121': 'Poverty',
+				  '127': 'Regulatory Reform',
+				  '133': 'Rural Policy',
+				  '139': 'Science and Space Policy',
+				  '151': 'Small Business',
+				  '145': 'Social Security',
+				  '157': 'Technology and Telecommunications',
+				  '163': 'Trade',
+				  '169': 'Transportation and Infrastructure',
+				  '175': 'Urban Policy',
+				  '181': 'Veterans and Military Families',
+				  '187': "Woman's Issues"};
+	*/
 
-	// Build an array of timestamps.
-	//   resolution = # of days between data points.
-	//   startDate = Unix Time Stamp of the earliest point on the graph.
-	var defineTimeRange = function(resolution, startDate) {
-		// Default arguments
-		resolution = resolution || 1; // Default value is 1 day.
-		startDate = startDate || 1314835200; // Default startDate is 01 Sep 2011 (just before WTP launched).
-		resValue = resolution * 86400; // Unix timestamp conversion
+	// A function that pulls in timeseries data from the Heroku API.
+	var loadTimeSeries = function(id) {
+		id = id || '';
+		path_id = '/' + id;
 
-		var nextTimestamp = new Date().getTime()/1000; // Store the current timestamp as the initial one.
-		while (nextTimestamp >= startDate) {
-			timestamps.push(nextTimestamp);
-			nextTimestamp = nextTimestamp - resValue; // Plot resolution of 1 week
-		}
-		timestamps = timestamps.reverse(); // Once all are in the array, arrange them in correct order.				
-	}
-
-	// Function which uses JSON to retrieve the petition results.
-	var loadPetitions = function(offset) {
-		offset = offset || 0;
-		$.getJSON(ENDPOINT + '?limit=1000&offset=' + offset + '&callback=?', function(data){
-			var resultset = data.metadata.resultset,
-			results = data.results;
-
-			petitionData = petitionData.concat(results);
-			if (resultset.count - offset > resultset.limit){
-				loadPetitions(resultset.offset + resultset.limit + 1);
-			} else {
-				$('body').trigger('petitionsLoaded');
-			}
-		});
-	};
-
-	var getRelatedContent = function(timestamp, issue_id) {
-		var related_content;
-		var json_url = 'http://lisabacker.com/wethepeople/?searchDate=' + timestamp + '&issueId=' + issue_id;
-		// Make API Request.
+		var path = timeSeriesAPI + path_id;
+	
 		$.ajax({
-    		url: json_url,
-    		type:'GET',
-    		dataType:'JSONP',
-    		success: function(data){
-    			var resultset = data.metadata.resultset,
-				related_content = data.results;
-				$('body').trigger('relatedContentLoaded');
-   			}
-		});
-		/*
-		$.getJSON(url, function(data){
-			var resultset = data.metadata.resultset,
-			related_content = data.results;
-		});
-		*/
+			type: 'GET',
+    		url: path,
+    		dataType:'jsonp',
+    		async: true,
+    		success: function(data, status){
+				var adjusted = [],
+				visibility;
+				
+				results = data;
+				$.each(results, function(index, issueObject){
 
-		return related_content;
-	}
+					if(issueObject.issue.id === 0) {
+						visibility = true;
+					} else if(issueObject.issue.id === id) {
+						visibility = true;
+					} else {
+						visibility = false;
+					}
 
-	// A loop to count open petitions on a given date.
-	var searchPetitions = function(timestamp, issue_name) {
-		issue_name = issue_name || 'All';
-		var count = 0;
-		var created;
-		var deadline;
+					issueObject.issue.visible = visibility;
+					adjusted.push(issueObject.issue);
+				});
 
-		$.each(petitionData, function(index, petitionsObject) {
+				timeSeriesData = adjusted;
+				buildChart(timeSeriesData);
+				$('img.loading').hide();
 
-			if (issue_name === 'All') {
-				created = petitionsObject['created'];
-				deadline = petitionsObject['deadline'];
-				if (created < timestamp && timestamp < deadline) {
-					count++;
-				}
-			} else {
-				if (petitionsObject.issues instanceof Array) {
-					$.each(petitionsObject.issues, function(issueIndex, issueObject) {
-						if(issueObject['name'] === issue_name) {
-							created = petitionsObject['created'];
-							deadline = petitionsObject['deadline'];
-							if (created < timestamp && timestamp < deadline) {
-								count++;
-							}
-						}
-					});
-				}
+			},
+			error: function(jqXHR, status, error) {
+				console.log('error: ' + error);
+				console.log('status: ' + jqXHR.status);
 			}
 		});
-
-		return count;
 	}
 
-	// Function for building the chart, given structured data.
+	// uses the contents of topPetitionData to build a list.
+	var buildRelevantResultsList = function(results) {
+		var petitionList = '',
+		petitionItem,
+		newsList = '',
+		newsItem;
+
+		// Hide loading wheel and populate new data.
+		$('img.loading-sm').hide();
+
+		if (results.success === true) {
+			$.each(results.data.articles, function(index, article) {
+				newsItem = '<li><a title="' + article.title + '" href="' + article.url + '">' + article.title + '</a></li>';
+				newsList += newsItem;
+			});
+			$.each(results.data.petitions, function(index, petition) {
+				petitionItem = '<li><a title="' + petition.title + '" href="' + petition.url + '">' + petition.title + '</a></li>';
+				petitionList += petitionItem;
+			});
+
+			$('.petition-list').append(petitionList);
+			$('.news-list').append(newsList);
+		} else {
+			$('.results').append('<h3>Oops. There was an error: ' + results.message + '</h3>')
+		}
+	}
+
+	// A function for building the chart, given structured data.
 	var buildChart = function(countData) {
 		$('#container').highcharts('StockChart', {
 		    chart: {
@@ -156,10 +134,14 @@ $(function(){
 
 		    legend: {
 		    	enabled: true,
+		    	verticalAlign: 'top',
+		    	align: 'center',
+		    	floating: false,
+		    	width: 770
 		    },
 
 		    title: {
-		        text: 'Petitions Count Over Time'
+		        text: null
 		    },
 
 		    scrollbar: {
@@ -181,33 +163,31 @@ $(function(){
                     point: {
                         events: {
                             click: function() {
-                            	console.log(this.series.name);
-                            	console.log(this.series.index);
-                            	console.log(this.x);
-                            	console.log(Highcharts.dateFormat('%A, %b %e, %Y', this.x));
+                            	$('img.loading-sm').show(); // Show mini loading wheel.
+
+                            	$('html, body').animate({
+        							scrollTop: $("#results-box").offset().top
+    							}, 2000);
+
+                            	console.log('Topic: ' + this.series.name); // Topic name.
+                            	console.log('Topic ID: ' + this.series.userOptions.id); // The ID to pass to getPetitionData.
+                            	console.log('Timestamp: ' + this.x); // The Timestamp to pass to getPetitionData.
 								
 								if ($(".results").is(":hidden")) {
 									$('.results').slideDown();
 								}
 
+								// Clear out all old data, and start populating new data.
 								$('.topic').empty();
-								$('.topic').append(this.series.name);
-
 								$('.date').empty();
+								$('.petition-list').empty();
+								$('.news-list').empty();
+
+								$('.topic').append(this.series.name);
 								$('.date').append(Highcharts.dateFormat('%A, %b %e, %Y', this.x));
 
-								var relatedContent = getRelatedContent(1369440000,22);
-								console.log(relatedContent);
-								/*
-								$.each(relatedContent, function(index, result){
-									// build out the list based on results.
-									$('.petition-list').empty();
-									$('news-list').empty();
-									
-
-								});
-								*/
-
+								// Call for new relevant data to be populated.
+								newsfinder.getPetitionData(this.x, this.series.userOptions.id, buildRelevantResultsList);
                             }
                         }
                     },
@@ -216,56 +196,10 @@ $(function(){
                     }
                 }
             },
-
 		    series: countData
 		});
 	}
 
-	loadPetitions();
-	defineTimeRange(1);
+	loadTimeSeries();
 
-	$('body').on('petitionsLoaded', function() {
-		var countLog = [];
-
-		// To prevent storing references to countLog.
-		function cloneCountLog(countLog) {
-			var clone = [];
-			for(var key in countLog){
-				if(countLog.hasOwnProperty(key)) {
-					clone[key] = countLog[key];
-				}
-			}
-			return clone;
-		}
-
-		// loop through the issues
-		$.each(issues, function(ind, issueName) {
-			countLog.length = 0; // clear out the count log for each issue.
-			var visibility = ((issueName === 'All') ? true : false);
-
-			// loop through the timestamps
-			$.each(timestamps, function(index, timestamp) {
-				var theCount = searchPetitions(timestamp, issueName);
-				countLog.push([timestamp*1000, theCount]);
-			});
-
-			allTrends.push({
-				name: issueName,
-				index: ind,
-				data: cloneCountLog(countLog),
-				visible: visibility
-			});
-
-
-		});
-
-		console.log(petitionData);
-		console.log(allTrends);
-		$('body').trigger('countCompleted');
-	});
-
-	$('body').on('countCompleted', function() {
-		$('img.loading').hide();
-		buildChart(allTrends);
-	});
 });
