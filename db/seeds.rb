8976247e2ef884c98e4da4ccb0667635bf6a2499
Ben Damman <ben.damman@gmail.com>
2013-06-13 21:12:12 -0500

    rake db:seeds output is more descriptive in log and console

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 # Update all petitions
+before = Petition.count
 Petition.update_all
+after = Petition.count
+puts message = "Petitions before and after: #{before} / #{after}"
+Rails.logger.info "DB:SEED ran: #{message}"
